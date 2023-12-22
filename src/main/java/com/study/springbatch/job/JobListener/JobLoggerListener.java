f5fe28b4c799e6f8894cca47898e14b8927e4a40
sujeong11 <qalzm7351@gmail.com>
2023-12-22 16:47:51 +0900

    feat: 배치 실행 전, 로그 추가를 위한 리스너

---
+++ src/main/java/com/study/springbatch/job/JobListener/JobLoggerListener.java
@@ -0,0 +1,31 @@
+package com.study.springbatch.job.JobListener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+
+@Slf4j
+public class JobLoggerListener implements JobExecutionListener {
+
+    private static String BEFORE_MASSAGE = "{} Job is Running.";
+
+    private static String AFTER_MASSAGE = "{} Job is Done. (Status: {})";
+
+    @Override
+    public void beforeJob(JobExecution jobExecution) {
+        log.info(BEFORE_MASSAGE, jobExecution.getJobInstance().getJobName());
+    }
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        log.info(AFTER_MASSAGE,
+            jobExecution.getJobInstance().getJobName(),
+            jobExecution.getStatus());
+
+        if (jobExecution.getStatus() == BatchStatus.FAILED) {
+            // 이메일 전송 작업
+            log.info("Job is Fail.");
+        }
+    }
+}
